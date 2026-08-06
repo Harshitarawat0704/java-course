255f5f944c2fb529967909ac7608830b5c82020e
Harshitarawat0704 <harshitarawat444@gmail.com>
2026-08-06 18:37:33 +0530

    ReverseArray

---
+++ day 24/Reverse.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        reverse(numbers);
+    }
+
+    private static void reverse(int[] array) {
+
+        System.out.println("Array = " + Arrays.toString(array));
+
+        int maxIndex = array.length - 1;
+
+        for (int i = 0; i < array.length / 2; i++) {
+
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
+}
