bdbf9ad4463736b4ee7b13bee03915f3266fa268
Harshitarawat0704 <harshitarawat444@gmail.com>
2026-08-16 08:02:08 +0530

    Added Second Largest program

---
+++ day 34/SecondLargest.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Second largest element does not exist.");
+        } else {
+            System.out.println("Largest = " + largest);
+            System.out.println("Second Largest = " + secondLargest);
+        }
+
+        scanner.close();
+    }
+}
