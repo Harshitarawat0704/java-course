ef98d54e4c86592d477a331604ef185094a79259
Harshitarawat0704 <harshitarawat444@gmail.com>
2026-08-16 07:52:41 +0530

    Added Sort 0 1 2 program

---
+++ day 34/Sort012.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Sort012 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter only 0, 1 and 2:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while (mid <= high) {
+
+            if (arr[mid] == 0) {
+
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+
+                low++;
+                mid++;
+
+            } else if (arr[mid] == 1) {
+
+                mid++;
+
+            } else {
+
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+
+                high--;
+            }
+        }
+
+        System.out.print("Sorted array: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
