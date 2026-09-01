3639bb48df950f8bff3e1e8e3671cfd82fde8648
Harshitarawat0704 <harshitarawat444@gmail.com>
2026-09-01 20:31:05 +0530

    Added Two Sum program

---
+++ day 49/TwoSum.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] + arr[j] == target) {
+
+                    System.out.println(
+                        "Pair: " + arr[i] + " + " + arr[j] + " = " + target
+                    );
+
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No pair found.");
+        }
+
+        sc.close();
+    }
+}
