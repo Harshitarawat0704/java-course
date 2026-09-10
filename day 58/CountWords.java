6b6eb559360f745aa9232c54b8db85308e60dcf5
Harshitarawat0704 <harshitarawat444@gmail.com>
2026-09-10 16:48:33 +0530

    Add Count Words program

---
+++ day 58/CountWords.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        // Remove extra spaces from beginning/end
+        sentence = sentence.trim();
+
+        if (sentence.isEmpty()) {
+            System.out.println("Number of words = 0");
+        } else {
+
+            // Split sentence wherever there are one or more spaces
+            String[] words = sentence.split("\\s+");
+
+            System.out.println("Number of words = " + words.length);
+        }
+
+        sc.close();
+    }
+}
