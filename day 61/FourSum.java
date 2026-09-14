55417875d83c0c3078c4f0d557e988357b9caa44
Harshitarawat0704 <harshitarawat444@gmail.com>
2026-09-14 19:55:01 +0530

    Add 4Sum program

---
+++ day 61/FourSum.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class FourSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 3; i++) {
+
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
+
+            for (int j = i + 1; j < arr.length - 2; j++) {
+
+                if (j > i + 1 && arr[j] == arr[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = arr.length - 1;
+
+                while (left < right) {
+
+                    long sum = (long) arr[i] + arr[j]
+                             + arr[left] + arr[right];
+
+                    if (sum == target) {
+
+                        System.out.println(
+                            "[" + arr[i] + ", " + arr[j] + ", "
+                            + arr[left] + ", " + arr[right] + "]"
+                        );
+
+                        left++;
+                        right--;
+
+                        while (left < right && arr[left] == arr[left - 1])
+                            left++;
+
+                        while (left < right && arr[right] == arr[right + 1])
+                            right--;
+
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+    }
+}
